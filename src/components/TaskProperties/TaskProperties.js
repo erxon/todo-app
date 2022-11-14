1d425de9d90dc7ada7ce1c963cf6109765211807
Ericson Castasus <castasuse@gmail.com>
2022-11-14 17:01:11 +0800

    modify TaskProperties

--- src/components/TaskProperties/TaskProperties.js
+++ src/components/TaskProperties/TaskProperties.js
@@ -1,9 +1,22 @@
 import React from "react";
+import dayjs from 'dayjs';
+import {Button} from "@mui/material";
 import { TextField } from "@mui/material";
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 function TaskProperties() {
+    //For Date and Time picker
+    const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'));
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+    };
+
     return <div>
         <div>
+            {/*Task properties fields*/}
             <TextField 
                 sx={{width: "25ch"}}
                 id="standard-basic" 
@@ -29,7 +42,20 @@ function TaskProperties() {
                 margin="dense"
                 multiline />
             <br />
-
+            {/*Date and Time picker*/}
+            <div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                        label="Date&Time picker"
+                        value={value}
+                        onChange={handleChange}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+            </div>
+            {/*Buttons - Add and Cancel*/}
+            <Button>Add</Button>
+            <Button>Cancel</Button>
         </div>
     </div>
 }
